c1abb75b0de60639eeb67d265af48832cf1d34de
Victor manuel Ninahuanca Ayala <mninahuanca3@gmail.com>
2019-09-09 19:00:45 -0400

    se creo la estructura basica de cada pagina del sistema, dividiendo sus partes en varios archivos

---
+++ src/pages/scripts.php
@@ -0,0 +1,22 @@
+
+  	<!-- jQuery 3 -->
+  	<script src="../../assets/vendor_components/jquery/dist/jquery.js"></script>
+
+  	<!-- jQuery UI 1.11.4 -->
+  	<script src="../../assets/vendor_components/jquery-ui/jquery-ui.js"></script>
+
+  	<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  	<script>
+  	  $.widget.bridge('uibutton', $.ui.button);
+  	</script>
+
+  	<script src="../../assets/vendor_components/popper/dist/popper.min.js"></script>
+  	<script src="../../assets/vendor_components/bootstrap/dist/js/bootstrap.js"></script>
+  	<script src="../../assets/vendor_components/jquery-slimscroll/jquery.slimscroll.js"></script>
+  	<script src="../../assets/vendor_components/fastclick/lib/fastclick.js"></script>
+  	<script src="../../assets/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
+  	<script src="../../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script>
+  	<script src="../../assets/vendor_components/jquery.peity/jquery.peity.js"></script>
+  	<script src="../js/template.js"></script>
+  	<script src="../js/pages/dashboard.js"></script>
+  	<script src="../js/demo.js"></script>
