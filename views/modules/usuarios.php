9d1acd3a49176bf721f6a2e0d39a821e6c70da24
Victor manuel Ninahuanca Ayala <mninahuanca3@gmail.com>
2019-09-10 18:18:28 -0400

    Se agrego el box principal para la lista de usuarios

--- views/modules/usuarios.php
+++ views/modules/usuarios.php
@@ -1,6 +1,27 @@
 
             <div class="content-wrapper">
+              <section class="content-header">
+                <h1>
+                  Usuarios
+                </h1>
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#"><i class="iconsmind-Library"></i></a></li>
+                  <li class="breadcrumb-item active">Todos los usuario</li>
+                </ol>
+              </section>
               <section class="content">
-
+                  <div class="row">
+                    <div class="col">
+                      <div class="box">
+                        <div class="box-header with-border">
+                				  <h3>Todos los usuarios</h3>
+                				</div>
+                        <div class="box-body">
+                          <div class="table-responsive">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
           	   </section>
             </div>
