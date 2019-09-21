fc4954505df10a9d28fbef419754823d45831c07
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2019-09-21 08:43:49 -0400

    se agrego el archivo gitignore

--- models/conexion.php
+++ models/conexion.php
@@ -8,7 +8,7 @@
       	private $charset = "utf8";
       	private $usuario = "root";
       	private $contrasena = "";
-      	private $unix_socket = "/var/run/mysql/mysql.sock";
+      	private $unix_socket = "/opt/lampp/var/mysql/mysql.sock";
       	public $pdo = null;
       	private $opciones = [PDO::ATTR_CASE => PDO::CASE_LOWER, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
       	function __construct()
