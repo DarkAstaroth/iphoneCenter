159ff1db0cd169760124f0f18a924727300866b9
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2019-09-22 15:14:05 -0400

    se convirtio en mayuscula los datos personales de la persona

--- models/crud.php
+++ models/crud.php
@@ -58,9 +58,9 @@ class datos extends conexion{
 
           $stmt->bindParam(1,$datosModel["ci"],PDO::PARAM_STR);
           $stmt->bindParam(2,$datosModel["idusuario"],PDO::PARAM_STR);
-          $stmt->bindParam(3,$datosModel["nombres"],PDO::PARAM_STR);
-          $stmt->bindParam(4,$datosModel["paterno"],PDO::PARAM_STR);
-          $stmt->bindParam(5,$datosModel["materno"],PDO::PARAM_STR);
+          $stmt->bindParam(3,strtoupper($datosModel["nombres"]),PDO::PARAM_STR);
+          $stmt->bindParam(4,strtoupper($datosModel["paterno"]),PDO::PARAM_STR);
+          $stmt->bindParam(5,strtoupper($datosModel["materno"]),PDO::PARAM_STR);
           $stmt->bindParam(6,$datosModel["telefono"],PDO::PARAM_STR);
           $stmt->bindParam(7,$datosModel["dir"],PDO::PARAM_STR);
 
